6eb6b4aad70973f2233802f171ea4d9ea97a169e
René Bärnreuther <rene.baernreuther@web.de>
2024-10-05 18:58:45 +0200

    Suchfunktion implementiert

---
+++ functions.js
@@ -0,0 +1,25 @@
+function filterCards() {
+    var input = document.getElementById("mtg-search");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("mtg-table");
+    var tr = table.getElementsByTagName("tr");
+    
+    // Starting at 1 to ignore the headers
+    for (var i = 1; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName("td");
+        if (td) {
+            var foundMatch = false;
+            for (var j = 0; j < td.length; j++) {
+                var txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    foundMatch = true;
+                } 
+            }
+            if(foundMatch) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
